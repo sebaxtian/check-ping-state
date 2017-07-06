961dfb4ec5a6395083367c25ee20cf31bc5ff49b
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2017-07-06 15:13:57 -0500

    Se guardan mensajes sobre archivos de Log

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const ping = require('ping');
 const nodemailer = require('nodemailer');
 const moment = require('moment');
 const gmail = require("./credenciales/gmail.js");
+const fs = require('fs');
+
+
+// Path del directorio de archivos Log
+let pathDirLogs = './logs/';
+// Archivo de Log
+let logFile = 'check-0.log';
+// Configuracion de archivo Log
+loadFileLog();
+
 
 // Lista de servidores objetivo para realizar peticiones
 let targetServers = ['164.132.176.183', '164.132.176.179'];
@@ -10,28 +20,22 @@ let targetServers = ['164.132.176.183', '164.132.176.179'];
 function checkState(server) {
     ping.promise.probe(server).then(response => {
         //console.log('response: ' + Object.keys(response));
-        console.log('----------------------------------------------------');
-        console.log('response.host: ' + response.host);
-        console.log('response.alive: ' + response.alive);
-        console.log('response.numeric_host: ' + response.numeric_host);
-        console.log('----------------------------------------------------');
+        // Configura mensaje para Log
+        let mensaje = '----------------------------------------------------\n';
+        mensaje += 'Hora Local: ' + moment().format('MMMM Do YYYY, h:mm:ss a') + '\n';
+        mensaje += 'response.host: ' + response.host + '\n';
+        mensaje += 'response.alive: ' + response.alive + '\n';
+        mensaje += 'response.numeric_host: ' + response.numeric_host + '\n';
+        mensaje += '----------------------------------------------------\n\n';
+        console.log(mensaje);
+        // Escribe sobre archivo de Log
+        saveLog(mensaje);
         // Valida si el servidor responde, esta vivo
         if(!response.alive) {
             console.log('El servidor no responde, no esta vivo.');
             // Configurar mensaje para notificar mediante un Email
             let para = 'sebaxtianrioss@gmail.com';
             let asunto = 'Check PING State';
-            let mensaje = 'La peticion al servidor no responde, no esta vivo:\n\n';
-            mensaje += 'Hora Local: ' + moment().format('MMMM Do YYYY, h:mm:ss a') + '\n\n';
-            mensaje += 'response.host: ' + response.host + '\n';
-            mensaje += 'response.alive: ' + response.alive + '\n';
-            mensaje += 'response.output: ' + response.output + '\n';
-            mensaje += 'response.time: ' + response.time + '\n';
-            mensaje += 'response.min: ' + response.min + '\n';
-            mensaje += 'response.max: ' + response.max + '\n';
-            mensaje += 'response.avg: ' + response.avg + '\n';
-            mensaje += 'response.stddev: ' + response.stddev + '\n';
-            mensaje += 'response.numeric_host: ' + response.numeric_host + '\n';
             mensaje += '\n\nPor favor valide si el servidor esta activo.\n\n';
             // Envia el mensaje
             sendEmail(para, asunto, mensaje);
@@ -64,6 +68,43 @@ function sendEmail(para, asunto, mensaje) {
         } else {
             console.log("Exito al enviar Email");
         }
+    });
+}
+
+// Funcion que configura el archivo de Log que sera escrito
+function loadFileLog() {
+    // Archivos del directorio de Logs ordenados por fecha de modificacion
+    let logFiles = fs.readdirSync(pathDirLogs);
+    logFiles.sort(function(a, b) {
+        return fs.statSync(pathDirLogs + a).mtime.getTime() - fs.statSync(pathDirLogs + b).mtime.getTime();
+    });
+    // Obtiene el ultimo archivo modificado
+    logFile = logFiles[logFiles.length - 1]
+    // Valida si existe el archivo
+    if(logFile != null) {
+        console.log('Ultimo archivo modificado: ' + logFile);
+        // Validar el tamanio del archivo
+        let stats = fs.statSync(pathDirLogs + logFile);
+        let fileSizeInBytes = stats.size;
+        console.log('File Size: ' + fileSizeInBytes + ' Bytes');
+        // 524288 == 0.5 MB
+        if(fileSizeInBytes > 524288) {
+            // El archivo es demasiado grande, se crea un nuevo archivo de log
+            var res = logFile.split("-")[1].split(".")[0];
+            res++;
+            logFile = 'check-' + res + '.log';
+        }
+    } else {
+        logFile = 'check-0.log';
+    }
+}
+
+// Funcion que escribe un mensaje sobre un archivo de Log
+function saveLog(mensaje) {
+    // Escribe el mensaje sobre el archivo de Log
+    fs.appendFile(pathDirLogs + logFile, mensaje, function (err) {
+        if (err) console.log('Error al escribir mensaje sobre archivo de Log ' + logFile);
+        console.log('Exito al escribir mensaje sobre archivo de Log ' + logFile);
     });
 }
 
