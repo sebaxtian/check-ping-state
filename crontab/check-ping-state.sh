5c253fcafb0f06643951950c4af9af37c558a41c
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2017-07-07 15:26:04 -0500

    Agrego configuracion para ejecutar desde Crontab Linux

---
+++ crontab/check-ping-state.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Script para ejecutar aplicacion Node check-ping-state
+#
+
+# 1. Carga el PATH donde se encuentra check-ping-state
+cd /path/to/node/check-ping-state/
+# 2. Ejecuta la aplicacion de Node check-ping-state
+node app.js
